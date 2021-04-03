efff3f928a92a787d855e25458431498c035e46f
Hafidz <9hafidz6@gmail.com>
2021-04-03 23:10:13 +0800

    worked on TCP and UDP servers as well as thier clients

---
+++ HTTP_servers.py
@@ -0,0 +1,77 @@
+import os
+import sys
+import socket
+from _thread import *
+import threading
+
+SERVER_ADDR = '127.0.0.1'
+UDP_SERVER_PORT = 1234
+TCP_SERVER_PORT = 4321
+
+#===============================================================================================================================
+
+def handle_UDPclient(udp_socket,data, addr):
+    #process the HTTP GET request here 
+    COMMAND, OBJECT, HTTP_VERSION = str(data.decode()).split('|')
+    print(f'udp server received: {COMMAND} {OBJECT} {HTTP_VERSION} from {addr}')
+    if COMMAND == "GET" and OBJECT == "/udp.html" and HTTP_VERSION == "HTTP/1.1":
+        message = (f"""<html>
+                    <head></head>
+                    <body><p>EE-4210: Continuous assessment</p></body>
+                    </html>""")
+        # message = 'HTTP/1.1 200 OK\n\nHello world'
+    udp_socket.sendto(message.encode(),addr)
+    sys.exit()
+
+#===============================================================================================================================
+
+def UDP_server():
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp_socket.bind((SERVER_ADDR,UDP_SERVER_PORT))
+    while True:
+        data, addr = udp_socket.recvfrom(1024)
+        start_new_thread(handle_UDPclient,(udp_socket,data,addr))
+        print(f"new UDP connection...")
+    udp_socket.close()   
+#===============================================================================================================================
+#==                                                                                                                           ==
+#==                                                                                                                           ==
+#==                                                                                                                           ==
+#===============================================================================================================================
+
+def handle_TCPclient(Client,addresss):
+    print(f"handling TCP client...")
+
+#===============================================================================================================================
+
+def TCP_server():
+    tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp_socket.bind((SERVER_ADDR,TCP_SERVER_PORT))
+    tcp_socket.listen(10)
+
+    while True:
+        Client, address = tcp_socket.accept()
+        print(f"connected to {address[0]} and {address[1]}")
+        start_new_thread(handle_TCPclient,(Client,address))
+        print(f'new TCP connection')
+    
+    tcp_socket.close()
+
+#===============================================================================================================================
+
+def main():
+    print(f"starting HTTP UDP server...")
+    t1 = threading.Thread(target=UDP_server,args=())
+    print(f"starting HTTP TCP server...")
+    t2 = threading.Thread(target=TCP_server,args=())
+
+    t1.start()
+    t2.start()
+
+    t1.join()
+    t2.join()
+
+    print(f'program ended')
+
+if __name__ == "__main__":
+    main()
--- test.py
+++ test.py
@@ -1,26 +1,49 @@
-import webbrowser
+# import webbrowser
+# import cgi
 
-def main():
-    f = open('test1.html', 'w')
-    message = """<html>
-                <head></head>
-                <body><p>Hello World!</p></body>
-                </html>"""
+# def main():
+#     # f = open('test1.html', 'w')
+#     # message = """<html>
+#     #             <head></head>
+#     #             <body><p>Hello World!</p></body>
+#     #             </html>"""
 
-    f.write(message)
-    f.close()
-    webbrowser.open('test1.html')
+#     # f.write(message)
+#     # f.close()
+#     webbrowser.open('test1.html')
 
-    while True:
-        user_msg = input('input user message here-> ')
-        f = open('test1.html', 'w')
-        message = (f"""<html>
-                    <head></head>
-                    <body><p>Hello World! {user_msg}</p></body>
-                    </html>""")
+#     while True:
+#         user_msg = input('input user message here-> ')
+#         f = open('test1.html', 'w')
+#         message = (f"""<html>
+#                     <head></head>
+#                     <body><p>Hello World!</p></body>
+#                     <form name="search" action="/cgi-bin/test.py" method="get">
+#                     Search: <input type="text" name="searchbox">
+#                     <input type="submit" value="Submit">
+#                     </form> 
+#                     </html>""")
+#         # form = cgi.FieldStorage()
+#         # searchterm =  form.getvalue('searchbox')
+#         # print(f"{searchterm}")
 
-        f.write(message)
-        f.close()
+#         f.write(message)
+#         f.close()
+
+# if __name__ == '__main__':
+#     main()
+
+from flask import Flask, request, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def my_form():
+    return render_template('test1.html')
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    variable = request.form['variable']
+    return variable
 
 if __name__ == '__main__':
-    main()
+    app.run(debug=True)
